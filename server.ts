0606ea63e69d3ee2d39a785fa4fa7fba339d6a15
vakot <vakotua.g@gmail.com>
2024-07-17 17:54:56 +0300

    fix: deploy

--- server.ts
+++ server.ts
@@ -3,10 +3,10 @@ import next from 'next'
 import { parse } from 'url'
 import { initializeSocket } from './modules/lib/websocket'
 
-const port = parseInt(process.env.PORT ?? '3000', 10)
-const hostname = process.env.HOSTNAME ?? 'localhost'
+// const port = parseInt(process.env.PORT ?? '3000', 10)
+// const hostname = process.env.HOSTNAME ?? 'localhost'
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, hostname, port })
+const app = next({ dev, hostname: 'localhost', port: 3000 })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
@@ -22,7 +22,8 @@ app.prepare().then(() => {
       console.error(err)
       process.exit(1)
     })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
+    .listen(3000, () => {
+      // console.log(`> Ready on http://${hostname}:${port}`)
+      console.log(`> Ready on http://'localhost':${3000}`)
     })
 })
