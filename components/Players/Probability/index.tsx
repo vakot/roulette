fd5ee118bdc0cf0427a2ebf6b633cc58e627d22f
vakot <vakotua.g@gmail.com>
2024-10-20 17:12:42 +0300

    feat: create & update UI atoms

---
+++ components/Players/Probability/index.tsx
@@ -0,0 +1,17 @@
+import { PlayerWithProbability } from '@components/Lists/BasePlayersList'
+import { useAdminPathname } from '@modules/hooks/useAdminPathname'
+import { Progress } from 'antd'
+
+export interface PlayerProbabilityProps {
+  player: PlayerWithProbability
+}
+
+export const PlayerProbability: React.FC<PlayerProbabilityProps> = ({ player }) => {
+  const isAdminPage = useAdminPathname()
+
+  if (!isAdminPage || player.probability === undefined || player.probability === null) {
+    return null
+  }
+
+  return <Progress type="circle" percent={Number((player.probability * 100).toFixed(1))} size={48} />
+}
