4f6ad9c40810d0a1c73e2844ef1658d61544a228
vakot <vakotua.g@gmail.com>
2024-10-20 15:12:01 +0300

    feat: create & update modals

---
+++ components/Buttons/AddPlayerButton/index.tsx
@@ -0,0 +1,37 @@
+import { PlusOutlined } from '@ant-design/icons'
+import { AddPlayerModal } from '@components/Modals/AddPlayerModal'
+import { IPlayer } from '@modules/models/Player'
+import { IRoulette } from '@modules/models/Roulette'
+import { Button, ButtonProps } from 'antd'
+import { useState } from 'react'
+
+export interface AddPlayerButtonProps extends ButtonProps {
+  players?: IPlayer[]
+  roulette?: IRoulette['_id']
+  onFinish?: (player: IPlayer) => void
+}
+
+export const AddPlayerButton: React.FC<AddPlayerButtonProps> = ({
+  roulette: rouletteId,
+  icon = <PlusOutlined />,
+  onClick,
+  onFinish,
+  ...props
+}) => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  return (
+    <>
+      <Button
+        type="dashed"
+        icon={icon}
+        onClick={(e) => {
+          onClick?.(e)
+          setOpen(true)
+        }}
+        {...props}></Button>
+
+      <AddPlayerModal open={open} roulette={rouletteId} onOk={() => setOpen(false)} onCancel={() => setOpen(false)} onFinish={onFinish} />
+    </>
+  )
+}
--- components/Forms/EditPlayerForm/index.tsx
+++ components/Forms/EditPlayerForm/index.tsx
@@ -1,11 +1,10 @@
-import { PlayerControlsCard } from '@components/Admin/Cards/PlayerControls'
+import { CloseOutlined } from '@ant-design/icons'
 import type { EditFormProps } from '@components/Forms/types'
 import { RouletteSelector } from '@components/Selectors/RouletteSelector'
 import { useAddPlayerMutation, useEditPlayerMutation, useGetPlayerQuery } from '@modules/api/player'
-import { useAdminPathname } from '@modules/hooks/useAdminPathname'
 import type { IPlayer } from '@modules/models/Player'
 import type { IRoulette } from '@modules/models/Roulette'
-import { Flex, Form, Input } from 'antd'
+import { Button, ColorPicker, Flex, Form, Input, Spin } from 'antd'
 import { useCallback, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -24,18 +23,12 @@ export const EditPlayerForm: React.FC<EditPlayerFormProps> = ({
 }) => {
   const [form] = Form.useForm(_form)
 
-  const { data: player } = useGetPlayerQuery(playerId, { skip: !playerId })
+  const { data: player, isLoading: isPlayerLoading } = useGetPlayerQuery(playerId, { skip: !playerId })
   const [editPlayer] = useEditPlayerMutation()
   const [addPlayer] = useAddPlayerMutation()
 
-  const { t } = useTranslation()
-
-  const isAdminPage = useAdminPathname()
-
   const handleFinish = useCallback(
     async (fields: Omit<IPlayer, '_id'>) => {
-      console.log(fields)
-
       try {
         const response = player ? await editPlayer({ ...player, ...fields }) : await addPlayer(fields)
 
@@ -62,47 +55,89 @@ export const EditPlayerForm: React.FC<EditPlayerFormProps> = ({
   }, [form, player, rouletteId])
 
   return (
-    <Form
-      onFinish={handleFinish}
-      form={form}
-      layout="vertical"
-      initialValues={{
-        roulette: rouletteId,
-      }}
-      {...props}
-    >
-      <Form.Item label={t('Name')}>
-        <Flex gap={8}>
-          <Form.Item name="name" style={{ flex: 1, margin: 0 }}>
-            <Input placeholder={t('Name')} />
-          </Form.Item>
-          {/* <Form.Item name="color" style={{ margin: 0 }}>
-            <ColorPicker />
-          </Form.Item> */}
-        </Flex>
-      </Form.Item>
-      <Form.Item label={t('Price')} name="price" required>
-        <Input type="number" placeholder={t('Price')} />
-      </Form.Item>
-      <Form.Item label={t('Message')} name="message">
-        <Input.TextArea rows={3} placeholder={t('Message-Optional')} />
-      </Form.Item>
-      <Form.Item label={t('Roulette')} name="roulette">
-        <RouletteSelector disabled={!!rouletteId} />
-      </Form.Item>
-      {isAdminPage && <EditPlayerFormControlsItem form={form} player={playerId} />}
-    </Form>
+    <Spin spinning={isPlayerLoading}>
+      <Form
+        onFinish={handleFinish}
+        form={form}
+        layout="vertical"
+        initialValues={{
+          roulette: rouletteId
+        }}
+        {...props}>
+        <FormNameColorItem form={form} player={playerId} />
+        <FormPriceItem form={form} player={playerId} />
+        <FormMessageItem form={form} player={playerId} />
+        <FormRouletteItem form={form} player={playerId} disabled={!!rouletteId} />
+      </Form>
+    </Spin>
   )
 }
 
-const EditPlayerFormControlsItem: React.FC<EditPlayerFormProps> = ({ form: _form, player: playerId }) => {
+const FormNameColorItem: React.FC<EditPlayerFormProps> = ({ form: _form, player: playerId, disabled = false }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Form.Item label={t('Name')}>
+      <Flex gap={8}>
+        <Form.Item name="name" style={{ flex: 1, margin: 0 }}>
+          <Input placeholder={t('Name')} />
+        </Form.Item>
+        <Form.Item name="color" style={{ margin: 0 }}>
+          <ColorPicker />
+        </Form.Item>
+      </Flex>
+    </Form.Item>
+  )
+}
+
+const FormPriceItem: React.FC<EditPlayerFormProps> = ({ form: _form, player: playerId, disabled = false }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Form.Item label={t('Price')} name="price" required>
+      <Input type="number" placeholder={t('Price')} />
+    </Form.Item>
+  )
+}
+
+const FormMessageItem: React.FC<EditPlayerFormProps> = ({ form: _form, player: playerId, disabled = false }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Form.Item label={t('Message')} name="message">
+      <Input.TextArea rows={3} placeholder={t('Message-Optional')} />
+    </Form.Item>
+  )
+}
+
+const FormRouletteItem: React.FC<EditPlayerFormProps> = ({ form: _form, player: playerId, disabled = false }) => {
   const [form] = Form.useForm(_form)
+
+  const { t } = useTranslation()
 
   const rouletteId = Form.useWatch('roulette', form)
 
+  const handleClear = useCallback(() => {
+    if (rouletteId) {
+      form.setFieldValue('roulette', null)
+    }
+  }, [form, rouletteId])
+
   return (
-    <Form.Item>
-      <PlayerControlsCard size="small" player={playerId} roulette={rouletteId} />
+    <Form.Item label={t('Roulette')}>
+      <Flex gap={8}>
+        <Form.Item name="roulette" noStyle>
+          <RouletteSelector disabled={disabled} />
+        </Form.Item>
+        <Button
+          icon={<CloseOutlined />}
+          type="primary"
+          danger
+          style={{ aspectRatio: 1 }}
+          onClick={handleClear}
+          disabled={disabled || !rouletteId}
+        />
+      </Flex>
     </Form.Item>
   )
 }
